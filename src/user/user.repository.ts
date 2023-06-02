67042458732fe6f8d48d9eb0b3d96f49b5272762
isaiasramosdeoliveira <isaias.ramos.olive@gmail.com>
2023-06-02 20:20:16 -0300

    fix: functional CRUD

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,27 +2,27 @@ import { Injectable } from "@nestjs/common";
 import { UserEntity } from "./user.entity";
 
 @Injectable()
-export class UserRepository{
+export class UserRepository {
     public dices: UserEntity[] = [];
-    
-    async salve(dice: UserEntity){
+
+    async salve(dice: UserEntity) {
         this.dices.push(dice);
     }
-    async listUser(){
+    async listUser() {
         return this.dices
     }
-    async listUserPerEmail(email: string){
+    async listUserPerEmail(email: string) {
         const possibleUser = this.dices.find(dice => email == dice.email);
         return possibleUser !== undefined;
     }
-    async update(id: string, dice: Partial<UserEntity>){
+    async update(id: string, dice: Partial<UserEntity>) {
         const possibleUser = this.dices.find(
             user => user.id === id
         )
-        if(!possibleUser) throw new Error("Usuário não existe.");
+        if (!possibleUser) { throw new Error("Usuário não existe."); }
 
         Object.entries(dice).forEach(([key, value]) => {
-            if(key === 'id'){
+            if (key === 'id') {
                 return;
             }
             possibleUser[key] = value
@@ -30,9 +30,17 @@ export class UserRepository{
 
         return possibleUser
     }
-    async delete(id: string){
+    private searchPerID(id: string) {
+        const possibleUser = this.dices.find(
+            user => user.id === id
+        )
+        if (!possibleUser) { throw new Error("Usuário não existe."); }
+        return possibleUser;
+    }
+    async delete(id: string) {
+        const user = this.searchPerID(id)
         const index = this.dices.findIndex(user => user.id === id);
-        if (!index) throw new Error("Usuário não existe.");
+        if (!user) throw new Error("Usuário não existe.");
         this.dices.splice(index, 1);
     }
 }
