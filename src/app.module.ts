85dd5d4c92df7779284329c15790339fa2a38815
isaiasramosdeoliveira <isaias.ramos.olive@gmail.com>
2023-06-26 14:29:48 -0300

    refactor: error handling

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, ClassSerializerInterceptor } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core'
+import { HttpExceptionFilter } from './common/filters/HttpExceptionFilter.filter';
 
 @Module({
   imports: [UserModule],
   controllers: [],
-  providers: [],
+  providers: [
+    { 
+      provide: APP_FILTER, 
+      useClass: HttpExceptionFilter 
+    }, {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
