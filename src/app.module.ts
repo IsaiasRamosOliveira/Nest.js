523f5cfa579159c42c34538d8f1d6d09bfa581bb
Isaías Ramos <isaias.ramos.olive@gmail.com>
2023-07-21 07:29:06 -0300

    refactor: docker-compose done

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { UserModule } from './user/user.module';
 import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core'
 import { HttpExceptionFilter } from './common/filters/HttpExceptionFilter.filter';
 import { TransformResponseInterceptor } from './core/http/TransformResponse.interceptor';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
+  ],
   controllers: [],
   providers: [
     { 
--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -1,15 +1,20 @@
-import { TypeOrmOptionsFactory } from "@nestjs/typeorm";
-import { TypeOrmModuleOptions } from "@nestjs/typeorm/dist";
+import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common'
 
-export class PostgresConfigService implements TypeOrmOptionsFactory{
+@Injectable()
+export class PostgresConfigService implements TypeOrmOptionsFactory {
+    constructor(
+        private configServices: ConfigService
+    ) { }
     createTypeOrmOptions(connectionName?: string): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
         return {
-            type:'postgres',
-            host: '127.0.0.1',
-            port: 5432,
-            username: "root",
-            password: "root",
-            database: "loja",
+            type: 'postgres',
+            host: this.configServices.get<string>('DB_HOST'),
+            port: this.configServices.get<number>('DB_PORT'),
+            username: this.configServices.get<string>('DB_USERNAME'),
+            password: this.configServices.get<string>('DB_PASSWORD'),
+            database: this.configServices.get<string>('DB_NAME'),
             entities: [],
             synchronize: true
         }
